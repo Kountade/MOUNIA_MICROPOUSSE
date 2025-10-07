3ee66d86ba492efd3a5f3d1e6db92b187f7b8849
Codons-ensemble <bounamakountagoudiaby@gmail.com>
2025-10-07 21:31:33 +0100

    fix allowed hosts for render

--- mounia/settings.py
+++ mounia/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = "django-insecure-1=(!k99p8!%&^b&zagsh^aetfl^9y(u)g#^_)h1lyi8$^h2kiu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mounia-micropousse.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
